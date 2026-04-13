a7a6d859f92e62c3ab16fd4d84d2f42710a75795
Raktim Koner <raktimkoner500@gmail.com>
2026-04-13 12:49:46 +0530

    new chenges fromed

--- api/index.js
+++ api/index.js
@@ -1,9 +1,31 @@
-import express from "express";
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Backend running");
-});
+// ✅ CORS
+app.use(cors({
+  origin: "https://pro-client-crm.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
-export default app;
+app.use(express.json());
+
+// routes
+const userRoutes = require("../routes/userRoutes");
+const reportRoutes = require("../routes/reportRoutes");
+const accountRoutes = require("../routes/accountRoutes");
+const dealRoutes = require("../routes/dealRoutes");
+const projectRoutes = require("../routes/projectRoutes");
+const taskRoutes = require("../routes/taskRoutes");
+
+app.use("/", userRoutes);
+app.use("/", reportRoutes);
+app.use("/", accountRoutes);
+app.use("/", dealRoutes);
+app.use("/", projectRoutes);
+app.use("/", taskRoutes);
+
+// ✅ export for Vercel
+module.exports = app;
